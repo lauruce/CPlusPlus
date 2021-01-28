4a440ddc44f17cacde4f954c0ed25ce05e180827
lauruce <lauruce@163.com>
2021-01-28 22:17:47 +0800

    Create C++ project

---
+++ Float/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export CC=/usr/local/bin/clang
+export CXX=/usr/local/bin/clang++
+export CXXFLAGS=-stdlib=libc++
+
+export MAKE_3RD_INC_DIRS=/home/chengdu/WorkSpace/C++/boost_1_75_0/build/include
+export MAKE_3RD_LIB_DIRS=/home/chengdu/WorkSpace/C++/boost_1_75_0/build/lib
+
+test -e build/ || mkdir build
+echo "mkdir build"
+
+test -e bin/ || mkdir bin
+echo "mkdir bin"
+
+cd build/
+cmake ..
+make
