d207b351ce0ba568f5f380baab4ac77157d39628
lauruce <lauruce@163.com>
2021-02-24 21:48:24 +0800

    Make build work

--- Float/build.sh
+++ Float/build.sh
@@ -4,8 +4,8 @@ export CC=/usr/local/bin/clang
 export CXX=/usr/local/bin/clang++
 export CXXFLAGS=-stdlib=libc++
 
-export MAKE_3RD_INC_DIRS=/home/chengdu/WorkSpace/C++/boost_1_75_0/build/include
-export MAKE_3RD_LIB_DIRS=/home/chengdu/WorkSpace/C++/boost_1_75_0/build/lib
+export MAKE_3RD_INC_DIRS=/home/chengdu/WorkSpace/3rdParty/boost_1_75_0/build/include
+export MAKE_3RD_LIB_DIRS=/home/chengdu/WorkSpace/3rdParty/boost_1_75_0/build/lib
 
 test -e build/ || mkdir build
 echo "mkdir build"
